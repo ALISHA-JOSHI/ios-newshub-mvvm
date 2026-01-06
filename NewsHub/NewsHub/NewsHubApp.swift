d9efcf27e9ebf66e3efd2bf9b871a68ab6c877a2
ALISHA JOSHI KOLANGARA <79350314+ALISHA-JOSHI@users.noreply.github.com>
2026-01-06 23:05:41 +0100

    Initial Xcode project setup

---
+++ NewsHub/NewsHub/NewsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsHubApp.swift
+//  NewsHub
+//
+//  Created by ALISHA JOSHI KOLANGARA on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct NewsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
